e7193d366d3911d930b628d1c8dd9370916bec72
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2020-01-16 14:58:08 -0300

    Loads games routes.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/ariel17/minesweeper-API/config"
 	_ "github.com/ariel17/minesweeper-API/docs"
+	"github.com/ariel17/minesweeper-API/games"
 	"github.com/ariel17/minesweeper-API/status"
 	"github.com/gin-gonic/gin"
 	"github.com/swaggo/files"
@@ -15,6 +16,7 @@ func main() {
 
 func loadRoutes(r *gin.Engine) *gin.Engine {
 	status.LoadRoutes(r)
+	games.LoadRoutes(r)
 	url := ginSwagger.URL(config.PublicURL + "/swagger/doc.json") // The url pointing to API definition
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 	return r
